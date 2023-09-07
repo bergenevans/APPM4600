01fe61207ae46cfd2e1e3d0926ba0036e692e75b
Bergen Evans <bergenevans@Bergens-MacBook-Pro.local>
2023-09-07 00:13:38 -0600

    Added directories from lab 1

---
+++ Labs/Lab 1/lab1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(0, 2 * np.pi, 100)
+Ya = np.sin(X)
+Yb = np.cos(X)
+
+plt.plot(X, Ya)
+plt.plot(X,Yb)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+x = np.linspace(1,10,10)
+y = np.arange(1,11)
+
+N = 3
+print('the first three entries of x are',x[:N])
+w = 10**(-np.linspace(1,10,10))
+print(w)
+
+l = len(w)
+
+x = np.linspace(1,l,10)
+
+s = 3 * w
+
+
+plt.semilogy(x,w)
+plt.semilogy(x,s)
+plt.xlabel('x')
+plt.ylabel('w/s')
+plt.show()
